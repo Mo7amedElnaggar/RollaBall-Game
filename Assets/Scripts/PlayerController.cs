7fca25257eb8d819a39dd5165ee2d5ea02ded563
Mo7amedElnaggar <NaggarQ@gmail.com>
2017-02-20 20:34:35 +0200

    Roll a Ball Game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI ;
+
+public class PlayerController : MonoBehaviour {
+	public float speed ;
+	private Rigidbody rb;
+	private int Count ;
+	public Text CubesCount , WinGame ;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		Count = 0;
+		setTextCount ();
+		WinGame.text = ""; // default 
+	}
+
+	void FixedUpdate(){ 
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+		
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);  
+			Count++;
+			setTextCount ();
+		}
+	}
+
+	void setTextCount(){
+		CubesCount.text = "Cubes Count : " + Count.ToString();
+		if (Count == 11)
+			WinGame.text = "Congratulation , u Collect all Cubes :D";
+	}
+}
